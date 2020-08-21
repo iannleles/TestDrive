4f8640d191f2aa1329b981c86537f1c6910af816
Iann Leles <iannleles@outlook.com>
2020-08-21 00:43:27 -0300

    Terminando aula 2, criando ListView ItemTapped e Binding

--- TestDrive/TestDrive/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ TestDrive/TestDrive/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace TestDrive {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.ListView listViewPaises;
+        private global::Xamarin.Forms.ListView ListViewVeiculos;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            listViewPaises = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listViewPaises");
+            ListViewVeiculos = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "ListViewVeiculos");
         }
     }
 }
